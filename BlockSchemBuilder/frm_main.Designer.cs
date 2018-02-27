2d4e18f3f79bf0c35e7d9b7f04b54c061b31b2d0
BloodDraon <36885560+BloodDraon@users.noreply.github.com>
2018-02-27 16:28:48 +0200

    redesigned

--- BlockSchemBuilder/frm_main.Designer.cs
+++ BlockSchemBuilder/frm_main.Designer.cs
@@ -40,17 +40,19 @@
             // 
             // txtBox_code
             // 
-            this.txtBox_code.Location = new System.Drawing.Point(13, 27);
+            this.txtBox_code.Location = new System.Drawing.Point(17, 33);
+            this.txtBox_code.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtBox_code.Multiline = true;
             this.txtBox_code.Name = "txtBox_code";
-            this.txtBox_code.Size = new System.Drawing.Size(350, 422);
+            this.txtBox_code.Size = new System.Drawing.Size(465, 518);
             this.txtBox_code.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(370, 27);
+            this.button1.Location = new System.Drawing.Point(497, 33);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(302, 23);
+            this.button1.Size = new System.Drawing.Size(399, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -58,11 +60,13 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.файлToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(684, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(912, 28);
             this.menuStrip1.TabIndex = 2;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -71,13 +75,13 @@
             this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.открытьcsToolStripMenuItem});
             this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(57, 24);
             this.файлToolStripMenuItem.Text = "Файл";
             // 
             // открытьcsToolStripMenuItem
             // 
             this.открытьcsToolStripMenuItem.Name = "открытьcsToolStripMenuItem";
-            this.открытьcsToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.открытьcsToolStripMenuItem.Size = new System.Drawing.Size(162, 26);
             this.открытьcsToolStripMenuItem.Text = "Открыть .cs";
             this.открытьcsToolStripMenuItem.Click += new System.EventHandler(this.открытьcsToolStripMenuItem_Click);
             // 
@@ -85,21 +89,23 @@
             // 
             this.flp_metodsList.AutoScroll = true;
             this.flp_metodsList.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flp_metodsList.Location = new System.Drawing.Point(373, 56);
+            this.flp_metodsList.Location = new System.Drawing.Point(497, 69);
+            this.flp_metodsList.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.flp_metodsList.Name = "flp_metodsList";
-            this.flp_metodsList.Size = new System.Drawing.Size(299, 393);
+            this.flp_metodsList.Size = new System.Drawing.Size(399, 484);
             this.flp_metodsList.TabIndex = 0;
             // 
             // frm_main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.ClientSize = new System.Drawing.Size(912, 567);
             this.Controls.Add(this.flp_metodsList);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtBox_code);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frm_main";
             this.Text = "frm_main";
             this.menuStrip1.ResumeLayout(false);
